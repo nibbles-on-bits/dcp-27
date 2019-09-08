d1bf0462e108ea496af56010cf798791e569f0f1
Chris Ash <goodvibesjumper@hotmail.com>
2019-09-08 10:59:06 -0600

    wip

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("DCP-27")
+
+	pba := [][2]int{}	// holds parenthesis pairs
+	cba := [][2]int{}	// holds curly brace pairs
+	sba := [][2]int{}	// holds square bracket pairs
+
+	tStr :="((){[]}()[{((()))}])"
+
+	for x := 0 ;x < {
+		tmp := 0
+		if tStr[x] := "(" {
+			tmp ++
+		}
+		
+
+	}
+
+
+}
