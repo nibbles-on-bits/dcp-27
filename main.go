a0c22ed9f5808e8c732a9c1e2f82e87d6b5578af
Chris Ash <goodvibesjumper@hotmail.com>
2019-09-08 15:40:46 -0600

    wip

--- main.go
+++ main.go
@@ -7,20 +7,32 @@ import (
 func main() {
 	fmt.Println("DCP-27")
 
-	pba := [][2]int{}	// holds parenthesis pairs
-	cba := [][2]int{}	// holds curly brace pairs
-	sba := [][2]int{}	// holds square bracket pairs
+	pba := [][2]int{} // holds parenthesis pairs
+	//cba := [][2]int{} // holds curly brace pairs
+	//sba := [][2]int{} // holds square bracket pairs
 
-	tStr :="((){[]}()[{((()))}])"
+	tStr := "((){[]}()[{((()))}])"
 
-	for x := 0 ;x < {
+	for x := 0; x < len(tStr); x++ {
 		tmp := 0
-		if tStr[x] := "(" {
-			tmp ++
+		if tStr[x] == '(' {
+			a := x
+			tmp++
+			for y := x + 1; y < len(tStr); y++ {
+				if tStr[y] == '(' {
+					tmp++
+				} else if tStr[y] == ')' {
+					tmp--
+					if tmp == 0 {
+						b := y
+						pba = append(pba, [2]int{a, b})
+						break
+					}
+				}
+			}
 		}
-		
-
 	}
 
+	fmt.Println(pba)
 
 }
